c59628ad61e7d247cac3c8f5cef57781a9a61d29
adgai19 <gaitondeaditya@gmail.com>
2022-03-06 17:06:55 +0530

    some cool things with cycle keymaps and moved snippets to own table

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -29,15 +29,8 @@ vim.keymap.set({ "i", "s" }, "<m-k>", function()
 	end
 end, { silent = true })
 
-local s = ls.s
-local fmt = require("luasnip.extras.fmt").fmt
-local i = ls.insert_node
-local rep = require("luasnip.extras").rep
+-- snippets
+local lua_snippets = require("adgai.snippets.lua").lua_snippets
+local all_snippets = require("adgai.snippets.all").snippets
 
--- snippest start here
-local lua_snippets = {
-	s("lr", fmt("local {} = require('{}')", { i(1, "package"), rep(1) })),
-	s("lf", fmt("local {}=function({})\n{}\nend", { i(1), i(2), i(3) })),
-}
-
-ls.snippets = { all = { ls.parser.parse_snippet("expand", "-- test snippet") }, lua = lua_snippets }
+ls.snippets = { all = all_snippets, lua = lua_snippets }
